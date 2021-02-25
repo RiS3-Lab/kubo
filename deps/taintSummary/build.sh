672489496134182473370466a289b8117f9032b3
Changming Liu <charley.ashbringer@gmail.com>
2021-02-24 21:57:48 -0500

    gcc9 doesn't work on taint summary

--- deps/taintSummary/build.sh
+++ deps/taintSummary/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir build && cd "$_"
-CC=gcc CXX=g++ cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$PWD/../../../llvm/bins-9.0" && make
+CC=gcc-5 CXX=g++-5 cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$PWD/../../../llvm/bins-9.0" && make
